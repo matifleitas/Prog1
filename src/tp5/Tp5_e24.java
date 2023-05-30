0cccf083c117b73bdf402492c257386a133aa4a3
Matias Fleitas <matifleitas6@gmail.com>
2023-05-30 14:20:25 -0300

    mas ejs

--- src/tp5/Tp5_e24.java
+++ src/tp5/Tp5_e24.java
@@ -2,30 +2,29 @@ package tp5;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Tp5_e24 {
+public class Tp5_e24{
+	
 	public static final int MAX = 20;
 	public static void main (String [] args) {
 		int [] arrint = {0, 0, 2, 3, 4, 0, 0, 0, 2, 3, 6, 7, 5, 0, 0, 3, 4, 5, 0, 0};
 		
-		int num = obt_num_user();
+		int num = obtenerPosicionValida();
 		obt_pos_de_sec(arrint, num);
 	}
 	public static void obt_pos_de_sec(int [] arrint, int num) {
 		int ini=0, fin=-1;
-		if(num >= MAX) {
-			System.out.println("Error");
-		}
+
 		if (arrint [num] == 0) {
-            ini = obt_ini_sec(arrint, fin + 1);
-            fin = obt_fin_sec(arrint, ini);
-        } else {
-            while (arrint[num] != 0) {
-                num++;
-            }
-            ini = obt_ini_sec(arrint, num);
-            fin = obt_fin_sec(arrint, ini);
+            System.out.println("No hay secuencia en esa pos");
+        } 
+		else {	
+	        while (arrint[num] != 0) {
+	            num--; //decremento hasta ini - 1, y asi poder aplicar obt_ini desde el principio
+	        }
+	        ini = obt_ini_sec(arrint, num);
+	        fin = obt_fin_sec(arrint, ini);
+	        imprimir_posiciones(ini, fin);
         }
-		imprimir_posiciones(ini, fin);
 	}
 	public static void imprimir_posiciones(int ini, int fin) {
 		System.out.println("La pos de inicio es: "+ini+" y la pos de fin es: "+fin);
@@ -54,4 +53,19 @@ public class Tp5_e24 {
 		}
 		return num;
 	}
+    public static int obtenerPosicionValida(){
+        int numero = -1;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+        while(numero < 1 || numero > MAX-2){
+            try{
+                System.out.println("Ingrese una posición mayor a 0 y menor a " + (MAX-1)+": ");
+                numero = Integer.valueOf(entrada.readLine()); 
+                System.out.println(numero);
+            }
+            catch(Exception exc ){
+                System.out.println( exc );
+            }
+        }
+        return numero;
+      }
 }
